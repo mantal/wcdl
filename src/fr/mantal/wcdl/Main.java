5f0430144669f1be8af57f1543e22f9e080bb603
mantal <mantal0@outlook.fr>
2016-06-21 17:43:15 +0200

    Add Dilbert downloader

--- src/fr/mantal/wcdl/Main.java
+++ src/fr/mantal/wcdl/Main.java
@@ -11,9 +11,11 @@ public class Main
 		Downloader phdcomics = new Downloader("#comic", "td[align=left]>a", "http://www.phdcomics.com/comics/", "archive.php?comicid=1", Paths.get(localPath, "PHD comics").toString()).Title("title", "PHD Comics: (.+)");
 		Downloader sinfest = new Downloader("td[colspan=\"2\"]>img", "table[border=\"0\"] td.style5:last-child>a", "http://www.sinfest.net/", "view.php?date=2000-01-17", Paths.get(localPath, "Sinfest").toString()).Title("td.style3", ".+: (.+)");
 		Downloader darkLegacy = new Downloader(".comic-image", ".nextLink", "http://www.darklegacycomics.com/", "1", Paths.get(localPath, "Dark Legacy").toString()).Title("title");
+		Downloader dilbert = new Downloader(".img-comic", ".nav-right>a", "http://dilbert.com/", "strip/1989-04-16", Paths.get(localPath, "Dilbert").toString()).Title(".comic-title-date").ImageFormat("gif");
 
 		phdcomics.download();
 		sinfest.download();
 		darkLegacy.download();
+		dilbert.download();
 	}
 }
