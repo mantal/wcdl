f9fa7f757e2e4cf584c6249096112e2b3e2ed0a7
mantal <mantal0@outlook.fr>
2016-06-21 17:41:51 +0200

    Fix DarkLegacy downloader

--- src/fr/mantal/wcdl/Main.java
+++ src/fr/mantal/wcdl/Main.java
@@ -10,7 +10,7 @@ public class Main
 
 		Downloader phdcomics = new Downloader("#comic", "td[align=left]>a", "http://www.phdcomics.com/comics/", "archive.php?comicid=1", Paths.get(localPath, "PHD comics").toString()).Title("title", "PHD Comics: (.+)");
 		Downloader sinfest = new Downloader("td[colspan=\"2\"]>img", "table[border=\"0\"] td.style5:last-child>a", "http://www.sinfest.net/", "view.php?date=2000-01-17", Paths.get(localPath, "Sinfest").toString()).Title("td.style3", ".+: (.+)");
-		Downloader darkLegacy = new Downloader(".comic-image", ".nextLink", "http://www.darklegacycomics.com/", "1", Paths.get(localPath + "Dark Legacy").toString()).Title("title");
+		Downloader darkLegacy = new Downloader(".comic-image", ".nextLink", "http://www.darklegacycomics.com/", "1", Paths.get(localPath, "Dark Legacy").toString()).Title("title");
 
 		phdcomics.download();
 		sinfest.download();
