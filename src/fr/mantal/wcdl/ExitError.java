55f10f0cd3a34bbface48f66ad7b5f766171035a
mantal <mantal0@outlook.fr>
2016-07-28 18:24:01 +0200

    Improve launch arguments

---
+++ src/fr/mantal/wcdl/ExitError.java
@@ -0,0 +1,6 @@
+package fr.mantal.wcdl;
+
+public class ExitError
+{
+	public static final int CONFIG_OPEN_ERROR = 0;
+}
--- src/fr/mantal/wcdl/Main.java
+++ src/fr/mantal/wcdl/Main.java
@@ -1,26 +1,22 @@
 package fr.mantal.wcdl;
 
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		String localPath = args.length > 1 ? args[0] : Paths.get(System.getProperty("user.home"), "Pictures", "Comics").toString();
+		Downloader.fromConfig(getPath(args[0])).download();
+	}
 
-		if (args.length >= 1)
-			Downloader.FromConfig(args[0]).download();
+	private static String getPath(String arg0)
+	{
+		if (arg0.isEmpty())
+			arg0 = System.getProperty("user.dir");
 
-		Downloader phdcomics = Downloader.FromConfig(Paths.get(localPath, "PHD comics").toString());
-		Downloader sinfest = Downloader.FromConfig(Paths.get(localPath, "Sinfest").toString());
-		Downloader darkLegacy = new Downloader(".comic-image", ".nextLink", "http://www.darklegacycomics.com/", "1", Paths.get(localPath, "Dark Legacy").toString()).Title(new Property("title"));
-		Downloader dilbert = new Downloader(".img-comic", ".nav-right>a", "http://dilbert.com/", "strip/1989-04-16", Paths.get(localPath, "Dilbert").toString()).Title(new Property(".comic-title-date")).ImageFormat("gif");
-		Downloader theDevilsPanties = Downloader.FromConfig(Paths.get(localPath, "The Devil's Panties").toString());
-
-		//phdcomics.download();
-		//sinfest.download();
-		//darkLegacy.download();
-		//dilbert.download();
-		//theDevilsPanties.download();
+		if (Files.isDirectory(Paths.get(arg0)))
+			return Paths.get(arg0, "config.wcdl").toString();
+		return arg0;
 	}
 }
